28fd6469f8514032473c6f779201a6c019a23384
Hanna Co <hannaco@ucla.edu>
2022-02-16 16:28:29 -0800

    client-server
    basic client server, no timers, multithreading, congestion control, or packet recovery implemented yet

--- client.c
+++ client.c
@@ -14,6 +14,8 @@ char* makeHeader(int32_t seq, int32_t ack, int16_t conn, int16_t flag) {
   int16_t mask_16 = 0b0000000011111111;
   int16_t temp;
   char* header;
+  int cwnd = 512;
+  int ssthresh = 10000;
   header = malloc(13);
   header[3] = seq&mask_32;
   header[2] = (seq>>8)&mask_32;
@@ -35,21 +37,72 @@ char* makeHeader(int32_t seq, int32_t ack, int16_t conn, int16_t flag) {
   return header;
 }
 
+int32_t getSeq(char* header) {
+  int32_t serv_seq = ((header[0]<<24)&0b11111111000000000000000000000000)
+                    +((header[1]<<16)&0b00000000111111110000000000000000)
+                    +((header[2]<<8)&0b00000000000000001111111100000000)
+                    +(header[3]&0b00000000000000000000000011111111);
+  return serv_seq;
+}
+
+int32_t getAck(char* header) {
+  int32_t serv_ack = ((header[4]<<24)&0b11111111000000000000000000000000)
+                    +((header[5]<<16)&0b00000000111111110000000000000000)
+                    +((header[6]<<8)&0b00000000000000001111111100000000)
+                    +(header[7]&0b00000000000000000000000011111111);
+  return serv_ack;
+}
+
+int16_t getFlags(char* header) {
+  int16_t serv_flag = ((header[10]<<8)&0b1111111100000000)+(header[11]&0b0000000011111111);
+  return serv_flag;
+}
+
+int16_t getConnection(char* header) {
+  int16_t connection = ((header[8]<<8)&0b1111111100000000)+(header[9]&0b0000000011111111);
+  return connection;
+}
+
+void printRecv(int16_t flag, int32_t serv_seq, int32_t serv_ack, int16_t connection, int cwnd, int ssthresh){
+  switch(flag) {
+    case 0: printf("RECV %d %d %d %d %d\n", serv_seq, serv_ack, connection, cwnd, ssthresh);
+      break;
+    case 2: printf("RECV %d %d %d %d %d SYN\n", serv_seq, serv_ack, connection, cwnd, ssthresh);
+      break;
+    case 4: printf("RECV %d %d %d %d %d ACK\n", serv_seq, serv_ack, connection, cwnd, ssthresh);
+      break;
+    case 1: printf("RECV %d %d %d %d %d FIN\n", serv_seq, serv_ack, connection, cwnd, ssthresh);
+      break;
+    case 6: printf("RECV %d %d %d %d %d ACK SYN\n", serv_seq, serv_ack, connection, cwnd, ssthresh);
+      break;
+    case 3: printf("RECV %d %d %d %d %d SYN FIN\n", serv_seq, serv_ack, connection, cwnd, ssthresh);
+      break;
+    case 5: printf("RECV %d %d %d %d %d ACK FIN\n", serv_seq, serv_ack, connection, cwnd, ssthresh);
+      break;
+    default: printf("RECV %d %d %d %d %d\n", serv_seq, serv_ack, connection, cwnd, ssthresh);
+  }
+}
+
 int main(int argc, char **argv)
 {
-  int i, portnum, s, sockfd, fileLength;
-  int16_t connection;
+  int i, portnum, n, sockfd, fileLength;
+  int16_t connection, serv_flag;
   int16_t SYN = 0b0000000000000010;
   int16_t ACK = 0b0000000000000100;
   int16_t FIN = 0b0000000000000001;
   int16_t NONE = 0b0000000000000000;
-  int32_t ack_num, seq_num;
+  int32_t MAX_SEQ = 0b00000000000000011001000000000000;
+  int32_t ack_num, seq_num, serv_seq, serv_ack;
   FILE* filePointer;
-  size_t res;
+  size_t res, itr, sendSize;
+  unsigned int len;
   char* sendbuf;
   char* header;
+  char buffer[525];
   struct hostent *server;
   struct sockaddr_in servaddr;
+  int cwnd = 512;
+  int ssthresh = 10000;
   // checking that we have both port number and file dir as args
   if(argc < 4) {
     fprintf(stderr, "ERROR: Not enough arguments to server.\n");
@@ -90,7 +143,7 @@ int main(int argc, char **argv)
   seq_num = 12345;
 
   // creating the header
-  header = makeHeader(seq_num, ack_num, connection, ACK);
+  header = makeHeader(seq_num, ack_num, connection, SYN);
 
   if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
   {
@@ -113,34 +166,117 @@ int main(int argc, char **argv)
   // copy ip address from server into servaddr
   memcpy(&servaddr.sin_addr.s_addr, server->h_addr, server->h_length);
 
-  char *hello = "Hello from client";
-
-  sendto(sockfd, (const char *)hello, strlen(hello), 0, (const struct sockaddr *) &servaddr,  sizeof(servaddr));
-
-  printf("Hello message sent.\n");
-           int n, len;
-           char buffer[1024];
-    n = recvfrom(sockfd, (char *)buffer, 1024, 
-                0, (struct sockaddr *) &servaddr,
-                &len);
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
-   
-    close(sockfd);
-    return 0;
-  
+  // send SYN to server
+  sendto(sockfd, (const char *)header, 12, 0, (const struct sockaddr *) &servaddr,  sizeof(servaddr));
+  printf("SEND %d %d %d %d %d SYN\n", seq_num, ack_num, connection, cwnd, ssthresh);
+  seq_num++;
   // receive SYN-ACK from server
+  n = recvfrom(sockfd, (char *)buffer, 525, 0, (struct sockaddr *) &servaddr, &len);
+  buffer[n] = '\0';
+  // decoding the header
+  serv_seq = getSeq(buffer);
+  serv_ack = getAck(buffer);
+  serv_flag = getFlags(buffer);
+  connection = getConnection(buffer);
+  printRecv(serv_flag, serv_seq, serv_ack, connection, cwnd, ssthresh);
+    
+
+  // for(int i = 0; i < 12; i++){
+  //   printf("%d\n", buffer[i]);
+  // }
 
   // send ACK with payload
+  ack_num = serv_seq+1;
+  header = makeHeader(seq_num, ack_num, connection, ACK);
+  memset(buffer, 0, sizeof buffer);
+  memcpy(buffer, header, 12);
+  if(res > 512){
+    memcpy(buffer+12, sendbuf, 512);
+    buffer[524] = '\0';
+    sendSize = 524;
+  } else {
+    memcpy(buffer+12, sendbuf, res);
+    buffer[res+12] = '\0';
+    sendSize = 12+res;
+  }
+  sendto(sockfd, (const char *)buffer, sendSize, 0, (const struct sockaddr *) &servaddr,  sizeof(servaddr));
+  printf("SEND %d %d %d %d %d ACK\n", seq_num, ack_num, connection, cwnd, ssthresh);
+  seq_num += sendSize - 12;
+  // receive ACK from server
+  n = recvfrom(sockfd, (char *)buffer, 525, 0, (struct sockaddr *) &servaddr, &len);
+  buffer[n] = '\0';
+  // decoding the header
+  serv_seq = getSeq(buffer);
+  serv_ack = getAck(buffer);
+  serv_flag = getFlags(buffer);
+  connection = getConnection(buffer);
+  printRecv(serv_flag, serv_seq, serv_ack, connection, cwnd, ssthresh);
+  ack_num = serv_seq+1;
+  
+
   // send file in 512 byte payloads (need to get ACKs)
+  itr = 512;
+  while(itr <= res){
+    header = makeHeader(seq_num, ack_num, connection, NONE);
+    memset(buffer, 0, sizeof buffer);
+    memcpy(buffer, header, 12);
+    memcpy(buffer+12, sendbuf+itr-1, 512);
+    buffer[524] = '\0';
+    sendSize = 524;
+    sendto(sockfd, (const char *)buffer, sendSize, 0, (const struct sockaddr *) &servaddr,  sizeof(servaddr));
+    printf("SEND %d %d %d %d %d\n", seq_num, ack_num, connection, cwnd, ssthresh);
+    n = recvfrom(sockfd, (char *)buffer, 525, 0, (struct sockaddr *) &servaddr, &len);
+    buffer[n] = '\0';
+    // decoding the header
+    serv_seq = getSeq(buffer);
+    serv_ack = getAck(buffer);
+    serv_flag = getFlags(buffer);
+    connection = getConnection(buffer);
+    seq_num += sendSize - 12;
+    ack_num = serv_seq+1;
+    printRecv(serv_flag, serv_seq, serv_ack, connection, cwnd, ssthresh);
+    itr+=512;
+  }
+  itr -= 512;
+  header = makeHeader(seq_num, ack_num, connection, NONE);
+  memset(buffer, 0, sizeof buffer);
+  memcpy(buffer, header, 12);
+  memcpy(buffer+12, sendbuf+itr-1, res-itr);
+  buffer[res-itr+12] = '\0';
+  sendSize = res-itr+12;
+  printf("%ld\n", sendSize);
+  sendto(sockfd, (const char *)buffer, sendSize, 0, (const struct sockaddr *) &servaddr,  sizeof(servaddr));
+  printf("SEND %d %d %d %d %d\n", seq_num, ack_num, connection, cwnd, ssthresh);
+  n = recvfrom(sockfd, (char *)buffer, 525, 0, (struct sockaddr *) &servaddr, &len);
+  buffer[n] = '\0';
+  // decoding the header
+  serv_seq = getSeq(buffer);
+  serv_ack = getAck(buffer);
+  serv_flag = getFlags(buffer);
+  connection = getConnection(buffer);
+  ack_num = serv_seq+1;
+  seq_num += sendSize - 12;
+  printRecv(serv_flag, serv_seq, serv_ack, connection, cwnd, ssthresh);
 
   // send FIN
+  header = makeHeader(seq_num, ack_num, connection, FIN);
+  sendto(sockfd, (const char *)header, 12, 0, (const struct sockaddr *) &servaddr,  sizeof(servaddr));
+  printf("SEND %d %d %d %d %d FIN \n", seq_num, ack_num, connection, cwnd, ssthresh);
+  seq_num++;
 
   // receive ACK
-
+  n = recvfrom(sockfd, (char *)buffer, 525, 0, (struct sockaddr *) &servaddr, &len);
+  buffer[n] = '\0';
+  // decoding the header
+  serv_seq = getSeq(buffer);
+  serv_ack = getAck(buffer);
+  serv_flag = getFlags(buffer);
+  connection = getConnection(buffer);
+  printRecv(serv_flag, serv_seq, serv_ack, connection, cwnd, ssthresh);
+  ack_num = serv_seq+1;
   // wait 2 seconds (receiving and ACKing FIN packets, drop others)
 
   // close connection
-
+  close(sockfd);
   exit(0);
 }
--- server.c
+++ server.c
@@ -8,6 +8,7 @@
 #include <signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <time.h>
 
 #define MAX_SIZE 524
 
@@ -42,19 +43,69 @@ char* makeHeader(int32_t seq, int32_t ack, int16_t conn, int16_t flag) {
   return header;
 }
 
+int32_t getSeq(char* header) {
+  int32_t serv_seq = ((header[0]<<24)&0b11111111000000000000000000000000)
+                    +((header[1]<<16)&0b00000000111111110000000000000000)
+                    +((header[2]<<8)&0b00000000000000001111111100000000)
+                    +(header[3]&0b00000000000000000000000011111111);
+  return serv_seq;
+}
+
+int32_t getAck(char* header) {
+  int32_t serv_ack = ((header[4]<<24)&0b11111111000000000000000000000000)
+                    +((header[5]<<16)&0b00000000111111110000000000000000)
+                    +((header[6]<<8)&0b00000000000000001111111100000000)
+                    +(header[7]&0b00000000000000000000000011111111);
+  return serv_ack;
+}
+
+int16_t getFlags(char* header) {
+  int16_t serv_flag = ((header[10]<<8)&0b1111111100000000)+(header[11]&0b0000000011111111);
+  return serv_flag;
+}
+
+int16_t getConnection(char* header) {
+  int16_t connection = ((header[8]<<8)&0b1111111100000000)+(header[9]&0b0000000011111111);
+  return connection;
+}
+
+void printRecv(int16_t flag, int32_t serv_seq, int32_t serv_ack, int16_t connection){
+  switch(flag) {
+    case 0: printf("RECV %d %d %d\n", serv_seq, serv_ack, connection);
+      break;
+    case 2: printf("RECV %d %d %d SYN\n", serv_seq, serv_ack, connection);
+      break;
+    case 4: printf("RECV %d %d %d ACK\n", serv_seq, serv_ack, connection);
+      break;
+    case 1: printf("RECV %d %d %d FIN\n", serv_seq, serv_ack, connection);
+      break;
+    case 6: printf("RECV %d %d %d ACK SYN\n", serv_seq, serv_ack, connection);
+      break;
+    case 3: printf("RECV %d %d %d SYN FIN\n", serv_seq, serv_ack, connection);
+      break;
+    case 5: printf("RECV %d %d %d ACK FIN\n", serv_seq, serv_ack, connection);
+      break;
+    default: printf("RECV %d %d %d\n", serv_seq, serv_ack, connection);
+  }
+}
+
 int main(int argc, char **argv)
 {
-  int i, portnum, sockfd, length;
-  int16_t connection;
+  int i, portnum, sockfd, length, milli_seconds;
+  clock_t start_time;
+  int16_t connection, cli_flag;
   int16_t SYN = 0b0000000000000010;
   int16_t ACK = 0b0000000000000100;
   int16_t FIN = 0b0000000000000001;
   int16_t NONE = 0b0000000000000000;
-  int32_t ack_num, seq_num;
+  int32_t ack_num, seq_num, cli_seq, cli_ack;
   unsigned int sz;
   int connection_count = 1;
   struct sockaddr_in my_addr, cli_addr;
   char buffer[MAX_SIZE + 1];
+  char* header;
+  FILE* filePointer;
+  char filename[4096];
   // checking that we have both port number and file dir as args
   if(argc < 3) {
     fprintf(stderr, "ERROR: Not enough arguments to server.\n");
@@ -102,15 +153,106 @@ int main(int argc, char **argv)
   // getting the size of cli_addr
   sz = sizeof(cli_addr);
 
-    // receiving packet from a client
-  length = recvfrom(sockfd, (char *) buffer, MAX_SIZE+1, 0, (struct sockaddr *) &cli_addr, &sz);
+  // receiving packet from a client
+  length = recvfrom(sockfd, (char *) buffer, MAX_SIZE, 0, (struct sockaddr *) &cli_addr, &sz);
   if (length < 0) {
     fprintf(stderr, "ERROR: error receiving packet - %s\r\n", strerror(errno));
     exit(1); 
   }
-
+  connection_count++;
+  snprintf(filename, sizeof(filename), ".%s/%d.file", argv[2], (connection_count-1));
+  filePointer = fopen(filename, "w+");
+  if(filePointer == NULL){
+    fprintf(stderr, "ERROR: Unable to open file.\n");
+    exit(1);
+  }
   // setting the last byte to zero so we can write to a file
   buffer[length] = '\0';
 
-  printf("Client : %s\n", buffer);
+  // getting seq and ack number from clients packet and making a response header
+  cli_seq = getSeq(buffer);
+  cli_ack = getAck(buffer);
+  cli_flag = getFlags(buffer);
+  seq_num = 4321;
+  ack_num = cli_seq+1;
+  header = makeHeader(seq_num, ack_num, connection_count, SYN+ACK);
+  printRecv(cli_flag, cli_seq, cli_ack, connection_count-2);
+
+  // sends SYN ACK, no payload
+  sendto(sockfd, (const char *)header, 12, 0, (const struct sockaddr *) &cli_addr, sz);
+  printf("SEND %d %d %d ACK SYN\n", seq_num, ack_num, connection_count-1);
+  seq_num++;
+
+  // Converting time into milli_seconds
+  milli_seconds = 1000 * 10;
+  // Storing start time
+  start_time = clock();
+
+  // waiting ten seconds
+  while(clock() < start_time+milli_seconds) {
+    // receives ACK with some payload
+    length = recvfrom(sockfd, (char *)buffer, MAX_SIZE, 0, (struct sockaddr *) &cli_addr, &sz);
+    if(length != -1)
+      break;
+  }
+  // restart timer
+  start_time = clock()
+  buffer[length] = '\0';
+  // decoding the header
+  cli_seq = getSeq(buffer);
+  cli_ack = getAck(buffer);
+  cli_flag = getFlags(buffer);
+  printRecv(cli_flag, cli_seq, cli_ack, connection_count-1);
+  if(cli_seq+length > 102400)
+    ack_num = cli_seq+length-12-102400-1;
+  else
+    ack_num = cli_seq+length-12;
+  fputs(buffer+12, filePointer);
+
+  // sends ACK, no payload
+  header = makeHeader(seq_num, ack_num, connection_count-1, ACK);
+  sendto(sockfd, (const char *)header, 12, 0, (const struct sockaddr *) &cli_addr, sz);
+  printf("SEND %d %d %d ACK\n", seq_num, ack_num, connection_count-1);
+  seq_num++;
+  if(cli_seq+length > 102400)
+    ack_num = cli_seq+length-12-102400-1;
+  else
+    ack_num = cli_seq+length-12;
+
+  // ACK received packets
+  do {
+    length = recvfrom(sockfd, (char *)buffer, MAX_SIZE, 0, (struct sockaddr *) &cli_addr, &sz);
+    buffer[length] = '\0';
+    fputs(buffer+12, filePointer);
+    // decoding the header
+    cli_seq = getSeq(buffer);
+    cli_ack = getAck(buffer);
+    cli_flag = getFlags(buffer);
+    if(cli_flag == FIN)
+      break;
+    printRecv(cli_flag, cli_seq, cli_ack, connection_count-1);
+      if(cli_seq+length > 102400)
+        ack_num = cli_seq+length-12-102400-1;
+      else
+        ack_num = cli_seq+length-12;
+    header = makeHeader(seq_num, ack_num, connection_count-1, ACK);
+    sendto(sockfd, (const char *)header, 12, 0, (const struct sockaddr *) &cli_addr, sz);
+    printf("SEND %d %d %d ACK\n", seq_num, ack_num, connection_count-1);
+    seq_num++;
+  } while(length != -1);
+
+  // receive FIN packet
+  printRecv(cli_flag, cli_seq, cli_ack, connection_count-1);
+  ack_num++;
+
+  // send ACK
+  header = makeHeader(seq_num, ack_num, connection_count-1, ACK);
+  sendto(sockfd, (const char *)header, 12, 0, (const struct sockaddr *) &cli_addr, sz);
+  printf("SEND %d %d %d ACK\n", seq_num, ack_num, connection_count-1);
+  seq_num++;
+
+  // send FIN
+
+  close(sockfd);
+  exit(0);
 }
